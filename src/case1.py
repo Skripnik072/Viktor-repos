460ce5f90e93aa2105e2d515c786e1ba6d7adce7
Виктор Скрипник <viktor_ds@mail.ru>
2025-03-17 21:13:20 +0800

    fix proba

---
+++ src/case1.py
@@ -0,0 +1,10 @@
+def filter_lists(mylist1: list[int], mylist2: list[int]) -> list[int]:
+    ''' Функция выводит список с общими числами'''
+    new_list = []
+    for i in mylist1:
+        if i in mylist2:
+            new_list.append(i)
+    return new_list
+
+
+print(filter_lists([1, 2, 3, 4], [3, 4, 5, 6]))
